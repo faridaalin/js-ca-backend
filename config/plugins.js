06335e7dee17c3e5fcc5f84d12204c55efc7461b
Farida Alin <farida.alin@yahoo.com>
2021-03-27 12:09:22 +0100

    Changes to my-project noted

--- config/plugins.js
+++ config/plugins.js
@@ -4,6 +4,7 @@ module.exports = {
     endpoint: "/graphql",
     shadowCRUD: true,
     playgroundAlways: true,
+    introspection: true,
     depthLimit: 7,
     amountLimit: 100,
     apolloServer: {
